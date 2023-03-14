8199ab218e21040414a4c56dbe8ceda01355b351
Krishen Mohan <krishenm@gmail.com>
2023-03-14 13:52:25 +0800

    experiments

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 text="src.diff"
-dir="./src"
+dir="./src/"
 
-awk '/^diff/{f="$dir"+substr($3,match($3,/[A-Za-z.]*(cpp|h)/))}{print>>f}' "$text"
-rg --files-without-match "new file mode" "$dir" | xargs -n1 rm
+awk '/^diff/{f=$dir substr($3,match($3,/[A-Za-z.]*(cpp|h)/))}{print>>f}' "$text"
+rg --files-without-match "new file mode" "$dir" | xargs echo
+rg --files-without-match "new file mode" "$dir" | xargs rm
+#rg --files "$dir" | xargs -n2 -I{} tail -n +6 {} >temp_{}
