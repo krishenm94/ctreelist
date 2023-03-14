acb0b40e0bd3bb9539d3c559edff4b07433f65cc
Krishen Mohan <krishenm@gmail.com>
2023-03-14 14:36:23 +0800

    extraction clean up

--- script.sh
+++ script.sh
@@ -7,5 +7,5 @@ rm -rf $dir
 mkdir $dir
 awk '/^diff/{f="./src/"substr($3,match($3,/[A-Za-z.]*(cpp|h)/))}{print>>f}' "$text"
 rg --files-without-match "new file mode" "$dir" | xargs rm
-rg --files "$dir" | xargs -I{} sed -ie '1,6d' {}
+rg --files "$dir" | xargs -I{} sed -i '1,6d' {}
 rg --files "$dir" | xargs -I{} sed -i 's/^+//g' {}
